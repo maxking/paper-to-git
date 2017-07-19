9a61bd23049c6112c024c714716042042872c3fa
Abhilash Raj <raj.abhilash1@gmail.com>
2017-07-18 19:44:41 -0700

    Add a basic sample test.

---
+++ paper_to_git/tests/bin/test_commands.py
@@ -0,0 +1,20 @@
+"""Tests for the main paper-git command line application."""
+
+import pytest
+from io import StringIO
+from unittest.mock import patch
+
+from paper_to_git.bin.paper_git import main
+
+
+class TestPaperGitCommand(object):
+    """Tests for the main paper-git command."""
+
+    def test_basic(self):
+        # Test that the command runs without any errors.
+        testargs = ['paper-git']
+        output = StringIO()
+        with patch('sys.argv', testargs), patch('sys.stdout', output):
+            with pytest.raises(SystemExit):
+                main()
+        assert 'usage' in output.getvalue()
