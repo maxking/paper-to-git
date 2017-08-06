e445ed0f7c57e488ea37b4423dd92b918d1378d9
Abhilash Raj <raj.abhilash1@gmail.com>
2017-08-05 18:57:13 -0700

    100% coverage for bin/paper_git.py.

--- papergit/tests/bin/test_commands.py
+++ papergit/tests/bin/test_commands.py
@@ -10,11 +10,31 @@ from papergit.bin.paper_git import main
 class TestPaperGitCommand(object):
     """Tests for the main paper-git command."""
 
-    def test_basic(self):
-        # Test that the command runs without any errors.
+    def test_usage_with_no_arguments(self):
+        # Test that the command runs without any arguments.
         testargs = ['paper-git']
         output = StringIO()
         with patch('sys.argv', testargs), patch('sys.stdout', output):
             with pytest.raises(SystemExit):
                 main()
         assert 'usage' in output.getvalue()
+
+    def test_usage_with_bad_arguments(self):
+        # Test that the command prints usage with wrong arguments.
+        testargs = ['paper-git', 'badcommand']
+        output = StringIO()
+        with patch('sys.argv', testargs), patch('sys.stderr', output):
+            with pytest.raises(SystemExit):
+                main()
+        assert 'usage' in output.getvalue()
+
+    def test_initialization(self):
+        # Test paper-git with a valid command.
+        testargs = ['paper-git', 'list']
+        output = StringIO()
+        error = StringIO()
+        with patch('sys.argv', testargs), \
+                patch('sys.stdout', output), patch('sys.stderr', error):
+            main()
+        assert '' in output.getvalue()
+        assert '' in error.getvalue()
